7685f253f1d29db6df9649d9960faf095ed224cd
tranquoc113 <tranquoc631@gmail.com>
2022-12-25 22:01:34 +0700

    ex io

--- app/IO/Demo1.hs
+++ app/IO/Demo1.hs
@@ -6,11 +6,26 @@
 
 import Data.Char
 
+-- main = do
+--     putStrLn("What's your firt name?")
+--     firtName <- getLine
+--     putStrLn("What's your last name?")
+--     lastName <- getLine
+--     let bigFirtName = map toUpper firtName
+--         bigLastName = map toUpper lastName
+--     putStrLn("Hello " ++ bigFirtName ++ " " ++ bigLastName) 
+
+
+
+-- let firstName = getLine
+
 main = do
-    putStrLn("What's your firt name?")
-    firtName <- getLine
-    putStrLn("What's your last name?")
-    lastName <- getLine
-    let bigFirtName = map toUpper firtName
-        bigLastName = map toUpper lastName
-    putStrLn("Hello " ++ bigFirtName ++ " " ++ bigLastName)    
+    line <- getLine
+    if null line
+        then return ()
+        else do
+            putStrLn(reverseWords line)
+            main
+
+reverseWords :: String -> String
+reverseWords xs = unwords (map reverse (words xs))
