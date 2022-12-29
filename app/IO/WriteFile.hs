80a61b204a2eb9b38404ef760300e753708b8c93
tranquoc113 <tranquoc631@gmail.com>
2022-12-29 23:02:56 +0700

    add remove item

--- app/IO/WriteFile.hs
+++ app/IO/WriteFile.hs
@@ -1,5 +1,7 @@
 import System.IO
 import Control.Monad
+import Data.List
+import System.Directory
 
 -- main = do
 --     handle <- openFile "girlfriend.txt" ReadMode
@@ -27,6 +29,22 @@ import Control.Monad
 --     todoItem <- getLine
 --     appendFile "todo.txt" (todoItem ++ "\n")
 
-main = forever $ do
-    line <- getLine
-    appendFile "todo.txt" (line ++ "\n")
+-- main = forever $ do
+--     line <- getLine
+--     appendFile "todo.txt" (line ++ "\n")
+
+main = do
+    contents <- readFile "todo.txt"
+    let todoTasks = lines contents
+        numberedTasks = zipWith(\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    -- mapM_ putStrLn todoTasks
+    mapM_ putStrLn(numberedTasks)
+    putStrLn("Wich one do you want to delete?")
+    numberString <- getLine
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    hPutStr tempHandle newTodoItems
+    hClose tempHandle
+    removeFile "todo.txt"
+    renameFile tempName "todo.txt" 
